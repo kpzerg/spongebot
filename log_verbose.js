5d042cf7c99a62affce69bfb4805617e864597c8
Case Regan <cp_regan@coloradocollege.edu>
2018-11-05 14:18:47 -0700

    removed unnecessary file

--- log_verbose.js
+++
@@ -1,7 +0,0 @@
-function log_v(message) {
-    var dt = new Date()
-    var date = dt.getFullYear()+'-'+(dt.getMonth()+1)+'-'+dt.getDate();
-    var time = dt.getHours()+':'+dt.getMinutes()+':'+dt.getSeconds();
-
-    console.log(date+' '+time+' '+message)
-}
