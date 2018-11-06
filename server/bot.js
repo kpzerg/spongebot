458fa95b0bd8f735ca77b198ba8d07ab576fe6c0
Kon Aoki <kon.aoki.us@gmail.com>
2018-11-05 19:36:01 -0700

    changed port again

--- server/bot.js
+++ server/bot.js
@@ -3,7 +3,7 @@ var auth = require('./auth.json');
 var net = require('net');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 2345;
 
 function log_v(message) {
     var dt = new Date();
