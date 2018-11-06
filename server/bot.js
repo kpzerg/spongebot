774730c018b907c3d69982b3176d247b1719165e
Kon Aoki <kon.aoki.us@gmail.com>
2018-11-05 22:41:39 -0700

    added pikachu feature

--- server/bot.js
+++ server/bot.js
@@ -4,6 +4,7 @@ var net = require('net');
 
 const hostname = '127.0.0.1';
 const port = 2345;
+var cid;
 
 function log_v(message) {
     var dt = new Date();
@@ -28,11 +29,70 @@ var server = net.createServer(function (conn) {
 
     bot.on('message', function (user, userID, channelID, message, evt) {
         log_v('sending message '+message)
+        cid=channelID;
         conn.write(JSON.stringify({ response: message , user: user}))
-
+        if (message[0]=='!') {
+            command_args = message.slice(1).replace(/\s/g,'').split('=');
+            process_command_arg(command_args[0], command_args[1]);
+        }
+        if(message.toLowerCase().includes("pikachu"))
+        {
+          var pikachu_kaomoji;
+          var ran = Math.floor(Math.random() * 7);
+          console.log(ran);
+          switch(ran)
+          {
+            case 0:
+              pikachu_kaomoji="\\**ヽ▼o・ェ・o▼ノ** pika!";
+              break;
+            case 1:
+              pikachu_kaomoji="▼o･∇･o▼ pika pika!";
+              break;
+            case 2:
+              pikachu_kaomoji="≦(o・⊥・o)≧ pikaa!";
+              break;
+            case 3:
+              pikachu_kaomoji="\\**ヽ▼o・▽・o▼ノ** pika pika!";
+              break;
+            case 4:
+              pikachu_kaomoji="▼o･∀･o▼ pika...";
+              break;
+            case 5:
+              pikachu_kaomoji="\\**ヽ▼o・∀・o▼ノ** pika!";
+              break;
+            case 6:
+              pikachu_kaomoji="▼o･◇･o▼ pika!?";
+              break;
+            case 7:
+              pikachu_kaomoji="\\**ヽ▼o・◇・o▼ノ** pika pikaa!";
+              break;
+          }
+          bot.sendMessage({
+              to: channelID,
+              message: pikachu_kaomoji
+          });
+        }
     });
 });
 
 server.listen(port, hostname, () => {
     log_v('listening at '+hostname+':'+port);
 });
+
+
+function process_command_arg(command, arg) {
+    switch(command) {
+        case 'help':
+            bot.sendMessage({
+                to: cid,
+                message: '```-!help for this message.\n-the bot will respond to any sentence with pikachu, including this\n-more upcoming features.```'
+            });
+            break;
+        default:
+            bot.sendMessage({
+                to: cid,
+                message: '```Not a valid command. Type !help for list of all commands supported.```'
+            });
+            break;
+    }
+}
